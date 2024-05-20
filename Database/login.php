26ec85b0d4dbb823beb7c26fae188757758acee9
Yuukii908 <156269429+Yuukii908@users.noreply.github.com>
2024-05-20 08:42:15 +0700

    Add files via upload

---
+++ Database/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="handler_register.php" method = "POST">
+      <div>
+    <label for="">email</label>
+    <input type="email" name = "email">
+</div>
+<div>
+    <label for="">password</label>
+    <input type="password" name = password>
+</div>
+<button type = "submit">Lanjutkan</button>  
+    </form>
+    <a href="register.php">Register</a>
+</body>
+</html>
---
+++ Database/register.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="handler_register.php" method = "POST">
+<div>
+    <label for="">id</label>
+    <input type="text"name = "id">
+</div>
+<div>
+    <label for="">nama</label>
+    <input type="text"name = "nama">
+</div>
+<div>
+    <label for="">email</label>
+    <input type="email"name = "email">
+</div>
+<div>
+    <label for="">password</label>
+    <input type="password"name = "password">
+</div>
+<button type = "submit">Lanjutkan</button>
+    </form>
+    <a href="login.php">Login</a>
+</body>
+</html>
